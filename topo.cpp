dc7320c0a43aff996fdad9faf0bbea6ecbf0846b
ductaip <dtai858@gmail.com>
2025-10-24 05:22:46 +0700

    fix

--- topo.cpp
+++ topo.cpp
@@ -1,4 +1,4 @@
-// #include <bits/stdc++.h>
+#include <bits/stdc++.h>
 #include <iostream>
 #include <vector>
 using namespace std;
@@ -14,7 +14,7 @@ vector<int> topo;
 
 void nhap() {
     cin >> n >> m;
-    visited.resize(n, false);
+    visited.resize(n+1, false);
     for(int i = 0; i < m; i++) {
         int x, y; cin >> x >> y;
         adj[x].push_back(y);
